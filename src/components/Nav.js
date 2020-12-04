d028a50551408511872fafce5de6c894a44be017
Snorre Nygaard <snorren.nygaard@finn.no>
2020-12-04 16:21:36 +0100

    Add styling for multi-level menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,13 +5,47 @@ import classes from './Nav.module.css';
 
 export function Nav() {
   return (
-    <nav className={classes.nav}>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/color">
-        <a>Color</a>
-      </Link>
+    <nav className={classes.sidenav}>
+      <div className={classes.sidenav__heading}>
+        [Tottallyawsome logo]
+      </div>
+      <ul>
+       <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <div className={classes.multilevel}>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Principles</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Changelog</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <Link href="/">
+          <a>Foundation</a>
+        </Link>
+        <div className={classes.multilevel}>
+          <ul>
+            <li>
+              <Link href="/color">
+                <a>Color</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      
+      </li>
+      </ul>
     </nav>
+    
   );
 }
