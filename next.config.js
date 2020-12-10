6a5425dd5523ad7da5480b0ef74ebb3fee7b2a49
Alexander Bjerkan <alexander.bjerkan@finn.no>
2020-12-10 13:17:52 +0100

    set basepath for GH pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
+
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withMDX({
+  // because we're serving this using GH pages
+  assetPrefix: isProd ? '/finn/fabric-design/' : undefined,
+  basePath: isProd ? '/finn/fabric-design' : undefined,
   pageExtensions: ['js', 'jsx', 'mdx'],
 });
