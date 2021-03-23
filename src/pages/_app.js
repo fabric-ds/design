b803e6d988de8479111aec97d9bdf6eb9ad81b6d
Dave Honneffer <pearofducks@gmail.com>
2021-03-23 10:34:49 +0100

    Fix Next goofiness

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,6 @@ import { Bad } from '../components/Bad';
 import { Good } from '../components/Good';
 import { Fig } from '../components/Fig';
 import { Nav } from '../components/Nav';
-import Image from 'next/image';
 import { AssetLink, AssetLinks } from '../components/AssetLink';
 import classes from '../components/App.module.css';
 import Footer from '../components/Footer';
@@ -17,7 +16,6 @@ const shortcodes = {
   Bad,
   Good,
   Fig,
-  Image: (props) => <Image {...props} />,
   h1: (props) => <h1 className="mt-32" {...props} />,
   h2: (props) => <h2 className="mt-32" {...props} />,
   h3: (props) => <h3 className="mt-32" {...props} />,
