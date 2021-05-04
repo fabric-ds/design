dad5476ae68dd3eae313cd8ed6e4438a493cb8d6
Richard <digitalsadhu@gmail.com>
2021-05-04 15:38:13 +0200

    use rehype plugins instead of remark

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-const toc = require('remark-toc');
-const slug = require('remark-slug');
+const slug = require("rehype-slug");
+const toc = require("@jsdevtools/rehype-toc");
 
-const withMDX = require('@next/mdx')({
+const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [slug, toc],
-  }
+    rehypePlugins: [slug, toc],
+  },
 });
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = withMDX({
   // because we're serving this using GH pages
-  assetPrefix: isProd ? '/finn/fabric-design/' : undefined,
-  basePath: isProd ? '/finn/fabric-design' : undefined,
-  pageExtensions: ['js', 'jsx', 'mdx'],
+  assetPrefix: isProd ? "/finn/fabric-design/" : undefined,
+  basePath: isProd ? "/finn/fabric-design" : undefined,
+  pageExtensions: ["js", "jsx", "mdx"],
 });
