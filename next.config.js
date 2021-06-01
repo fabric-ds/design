fbcae101ac0ce0846578d46d82a6b561077d139d
Richard <digitalsadhu@gmail.com>
2021-06-01 20:56:36 +0200

    fix: correct root path

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = withMDX({
   // because we're serving this using GH pages
-  assetPrefix: isProd ? "/finn/fabric-design/" : undefined,
-  basePath: isProd ? "/finn/fabric-design" : undefined,
+  assetPrefix: isProd ? "/fabric-design/" : undefined,
+  basePath: isProd ? "/fabric-design" : undefined,
   pageExtensions: ["js", "jsx", "mdx"],
 });
