35e86ca0345a438a288b4cbea18dd952cd0e1842
Richard <digitalsadhu@gmail.com>
2021-09-23 13:07:13 +0200

    feat: use fabric docs template

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Head from 'next/head';
-import { MDXProvider } from '@mdx-js/react';
-
-import { TableOfContents } from '../components/TableOfContents';
-import { Bad } from '../components/Bad';
-import { Good } from '../components/Good';
-import { Fig } from '../components/Fig';
-import { Nav } from '../components/Nav';
-import { Guideline } from '../components/Guideline';
-import { AssetLink, AssetLinks } from '../components/AssetLink';
-import classes from '../components/App.module.css';
-import Footer from '../components/Footer';
+import { MDXProvider } from "@mdx-js/react";
+import { TableOfContents } from "../components/TableOfContents";
+import { Bad } from "../components/Bad";
+import { Good } from "../components/Good";
+import { Fig } from "../components/Fig";
+import { Nav } from "../components/Nav";
+import { Guideline } from "../components/Guideline";
+import { AssetLink, AssetLinks } from "../components/AssetLink";
+import classes from "../components/App.module.css";
+import Footer from "../components/Footer";
 
 const shortcodes = {
   TableOfContents,
@@ -27,14 +26,26 @@ const shortcodes = {
   h5: (props) => <h5 className="mt-32" {...props} />,
   h6: (props) => <h6 className="mt-32" {...props} />,
   p: (props) => <p className="pb-16" {...props} />,
-  ul: (props) => <ul className="pb-16 list-disc list-inside" {...props} />, 
-  ol: (props) => <ol className="pb-16 list-decimal list-inside" {...props} />, 
-  dl: (props) => <dl className="pb-16" {...props} />, 
-  li: (props) => <li className="pb-4" {...props} />, 
+  ul: (props) => <ul className="pb-16 list-disc list-inside" {...props} />,
+  ol: (props) => <ol className="pb-16 list-decimal list-inside" {...props} />,
+  dl: (props) => <dl className="pb-16" {...props} />,
+  li: (props) => <li className="pb-4" {...props} />,
   table: (props) => <table className="mb-16 p-0 w-full" {...props} />,
-  tr: (props) => <tr className="border-t border-gray-300 bg-white m-0 p-0" {...props} />,
-  th: (props) => <th className="border border-gray-300 bg-white m-0 py-6 px-14 text-left" {...props} />,
-  td: (props) => <td className="border border-gray-300 bg-white m-0 py-6 px-14 text-left" {...props} />,  
+  tr: (props) => (
+    <tr className="border-t border-gray-300 bg-white m-0 p-0" {...props} />
+  ),
+  th: (props) => (
+    <th
+      className="border border-gray-300 bg-white m-0 py-6 px-14 text-left"
+      {...props}
+    />
+  ),
+  td: (props) => (
+    <td
+      className="border border-gray-300 bg-white m-0 py-6 px-14 text-left"
+      {...props}
+    />
+  ),
   pre: (props) => <pre className="pb-16" {...props} />,
   main: (props) => <main className="break-words" {...props} />,
   strong: (props) => <strong className="strong" {...props} />,
@@ -46,31 +57,36 @@ const shortcodes = {
 function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>FINN Fabric</title>
-      </Head>
-      <MDXProvider components={shortcodes}>
-        <div
-          className="grid h-screen"
-          style={{ gridTemplateColumns: '256px 1fr' }}
-        >
+      <f-docs-template>
+        <section slot="sidebar">
           <Nav />
-
+          {/* <f-docs-navigation></f-docs-navigation> */}
+        </section>
+        <section slot="navigation">
+          <f-docs-navigation></f-docs-navigation>
+        </section>
+        <section slot="content">
           <div
-            className="grid justify-center p-40"
-            style={{
-              gridTemplateColumns: 'minmax(auto, 900px)',
-              gridTemplateRows: '1fr auto',
-            }}
+            className="grid h-screen"
+            style={{ gridTemplateColumns: "256px 1fr" }}
           >
-            <main>
-              <Component {...pageProps} />
-            </main>
-
-            <Footer />
+            <div
+              className="grid justify-center p-40"
+              style={{
+                gridTemplateColumns: "minmax(auto, 900px)",
+                gridTemplateRows: "1fr auto",
+              }}
+            >
+              <MDXProvider components={shortcodes}>
+                <main>
+                  <Component {...pageProps} />
+                </main>
+                <Footer />
+              </MDXProvider>
+            </div>
           </div>
-        </div>
-      </MDXProvider>
+        </section>
+      </f-docs-template>
     </>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,10 +8,42 @@ class MyDocument extends Document {
             href="https://assets.finn.no/pkg/@finn-no/fabric-css/v0/fabric.min.css"
             rel="stylesheet"
           />
+          <link
+            href="https://assets.finn.no/pkg/@finn-no/fabric-common/v1/template.js"
+            rel="modulepreload"
+          />
+          <link
+            href="https://assets.finn.no/pkg/@finn-no/fabric-common/v1/asset-links.js"
+            rel="modulepreload"
+          />
+          <link
+            href="https://assets.finn.no/pkg/@finn-no/fabric-common/v1/sidebar.js"
+            rel="modulepreload"
+          />
+          <link
+            href="https://assets.finn.no/pkg/@finn-no/fabric-common/v1/navigation.js"
+            rel="modulepreload"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <script
+            src="https://assets.finn.no/pkg/@finn-no/fabric-common/v1/template.js"
+            type="module"
+          ></script>
+          <script
+            src="https://assets.finn.no/pkg/@finn-no/fabric-common/v1/asset-links.js"
+            type="module"
+          ></script>
+          <script
+            src="https://assets.finn.no/pkg/@finn-no/fabric-common/v1/sidebar.js"
+            type="module"
+          ></script>
+          <script
+            src="https://assets.finn.no/pkg/@finn-no/fabric-common/v1/navigation.js"
+            type="module"
+          ></script>
         </body>
       </Html>
     );
