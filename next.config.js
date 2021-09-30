ce5e2ea648abb750a8a30c8b17e7e34c73dc2f44
Richard <digitalsadhu@gmail.com>
2021-09-30 11:29:40 +0200

    chore: change back base url

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withTM(
   withMDX({
+    // because we're serving this using GH pages
+    assetPrefix: isProd ? '/design' : undefined,
+    basePath: isProd ? '/design' : undefined,
     pageExtensions: ['js', 'jsx', 'mdx'],
   }),
 );
