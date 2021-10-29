2c6010d3f07ec16806bf4740b910e7d1d18472b8
Richard <digitalsadhu@gmail.com>
2021-10-29 16:36:28 +0200

    docs: flesh out instruction snippet pages

--- src/pages/guide-instructions.js
+++ src/pages/guide-instructions.js
@@ -4,22 +4,47 @@ import { useRouter } from 'next/router';
 import GuideReact from './guide-react.mdx';
 import GuideCustomElements from './guide-custom-elements.mdx';
 import GuideEikNodeClient from './guide-eik-node-client.mdx';
+import GuideEikNodeClientJavascript from './guide-eik-node-client-javascript.mdx';
 import GuideExpressLayout from './guide-express-layout.mdx';
 import GuideExpressPodlet from './guide-express-podlet.mdx';
 import GuideHTMLTemplate from './guide-html-template.mdx';
 import GuideVue from './guide-vue.mdx';
+import GuideEik from './guide-eik.mdx';
+import GuideEikJavascript from './guide-eik-javascript.mdx';
+import GuideEikJavascriptReact from './guide-eik-javascript-react.mdx';
+import GuideEikJavascriptVue from './guide-eik-javascript-vue.mdx';
+import GuideEikJavascriptElements from './guide-eik-javascript-elements.mdx';
 
 export default function Instructions() {
   const router = useRouter();
-  const { react, vue, elements, nodeClient, htmlTemplate, layout, podlet } =
-    router.query;
+  const {
+    eik,
+    react,
+    vue,
+    elements,
+    nodeClient,
+    htmlTemplate,
+    layout,
+    podlet,
+  } = router.query;
   return (
     <>
       <Head>
         <title>Guide - Instructions | FINN Fabric</title>
       </Head>
-
+      {eik && <GuideEik></GuideEik>}
+      {eik && (react || vue || elements) && (
+        <GuideEikJavascript></GuideEikJavascript>
+      )}
+      {eik && react && <GuideEikJavascriptReact></GuideEikJavascriptReact>}
+      {eik && vue && <GuideEikJavascriptVue></GuideEikJavascriptVue>}
+      {eik && elements && (
+        <GuideEikJavascriptElements></GuideEikJavascriptElements>
+      )}
       {nodeClient && <GuideEikNodeClient></GuideEikNodeClient>}
+      {nodeClient && (react || vue || elements) && (
+        <GuideEikNodeClientJavascript></GuideEikNodeClientJavascript>
+      )}
       {layout && <GuideExpressLayout></GuideExpressLayout>}
       {podlet && <GuideExpressPodlet></GuideExpressPodlet>}
       {htmlTemplate && <GuideHTMLTemplate></GuideHTMLTemplate>}
