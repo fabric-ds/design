198d1b0564d751ed2823f9d401730da81adc60fd
Benjamin Akar <benjaminakar2001@gmail.com>
2021-11-04 15:39:05 +0100

    update theme name

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,9 +1,9 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { nord as dark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { nord } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export const CodeBlock = (props) => {
   return (
-    <SyntaxHighlighter language={props.className?.replace(/language-/, '')} style={dark}>
+    <SyntaxHighlighter language={props.className?.replace(/language-/, '')} style={nord}>
       {props.children}
     </SyntaxHighlighter>
   );
