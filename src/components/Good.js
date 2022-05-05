649de63efef5a4e147f0399858c6c3fa5f88da87
Ingrid Hagelund <101707306+Hagis89@users.noreply.github.com>
2022-05-05 16:04:41 +0200

    Refactored guidelines to accept good and bad images

--- src/components/Good.js
+++ src/components/Good.js
@@ -2,11 +2,13 @@ import React from 'react';
 import classes from '../components/Good.module.css';
 
 export function Good(props) {
-const{className, ...rest} = props
+  const { className, ...rest } = props;
   return (
-    <section 
+    <section
+      // className={`${classes.good} relative border-solid border-0 border-l-4 border-green-500 bg-gray-100 p-5 rounded-r flex align-middle justify-center`}
+      // {...props}
       className={`${classes.good} relative border-solid border-0 border-l-4 border-green-500 bg-gray-100 p-5 rounded-r flex align-middle justify-center ${className}`}
-      {...rest} 
+      {...rest}
     />
   );
 }
